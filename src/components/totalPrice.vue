bdd4083a51a9f48c4c0af85d1d19786070891b88
hosseinnourian <hosseinnourian@protonmail.com>
2024-08-07 16:11:23 +0330

    finish total price section

--- src/components/totalPrice.vue
+++ src/components/totalPrice.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <p>{{ totalPrice }}</p>
+    <div v-if="totalPrice != 0">
+        <p> Total Price : <span>{{ totalPrice }}</span></p>
     </div>
 </template>
 
@@ -15,9 +15,17 @@ div {
     background-color: white;
     padding: 10px;
     border-radius: 10px;
+    text-align: center;
+    line-height: normal;
 }
 
 p {
+    color: rgb(58, 55, 55);
+    font-weight: bold;
+    font-size: 20px;
+}
+
+span {
     color: red;
 }
 </style>
